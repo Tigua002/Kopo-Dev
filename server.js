ccf541a0f2c8977209a6e66383fb46ff5bb8ae83
Tigua002 <118343679+Tigua002@users.noreply.github.com>
2024-02-29 08:55:11 +0100

    Update server.js to discord

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
-
+require("dotenv").config()
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running on this port: ${PORT}`));
 
@@ -39,11 +39,27 @@ app.get('/', function (req, res) {
 })
 
 
+const { Client, IntentsBitField } = require('discord.js')
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent
+  ]
+})
+
+client.on('ready', (c) => {
+  console.log(`${c.user.tag} is online`)
+})
+
+client.login(process.env.TOKEN)
 
 
 app.post("/send/mail", function (req, res) {
   // skaffer user og passord fra data-en og gir dem en verdi
-  let message=req.body.message
+  let message = req.body.message
   let name = req.body.sender
   let email = req.body.email
   var mailOptions = {
@@ -52,35 +68,14 @@ app.post("/send/mail", function (req, res) {
     subject: `INCOMING MAIL FROM ${name}: ${email}`,
     text: message
   };
+
+
+  client.channels.fetch('1211988415881543710')
+    .then(channel => channel.send(message))
+  console.log("Discord succecfully notified")
+
   res.send(sendMail(transporter, mailOptions))
 })
 
-// // post "/create/user/"
-// app.post("/create/user/", function (req, res) {
-//     // skaffer user og passord fra data-en og gir dem en verdi
-//     let user = req.body.user
-//     let passord = req.body.passord
-//     let hiddenPass = md5(passord)
-//     // legger til brukeren i users "table"
-//     connection.query(`INSERT INTO clients (username, password) VALUES ("${user}", "${hiddenPass}")`)
-
-
-//     connection.query(`CREATE TABLE ${user}Connections (initiator VARCHAR(100), accepter VARCHAR(100), initiatorID INT, accepterID INT)`)
-
-
-
-// })
-
-
-// app.post("/change/user/", function (req, res) {
-//     // skaffer user og passord fra data-en og gir dem en verdi
-//     let oldUser = req.body.oldUser
-//     let newUser = req.body.newUsername
-//     let password = req.body.pass
-//     let hiddenPass = md5(password)
-//     // legger til brukeren i users "table"
-//     connection.query(`UPDATE clients set username = "${newUser}" WHERE username = "${oldUser}" AND password = "${hiddenPass}"`)
-//     connection.query(`ALTER TABLE ${oldUser}connections RENAME TO ${newUser}connections`)
-// })
 
 app.use(express.static("client"))
